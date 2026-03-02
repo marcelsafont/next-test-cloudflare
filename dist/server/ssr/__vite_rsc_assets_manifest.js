74f7cc9e458ff148bc471a9a2de5f7e59e0163b5
marcel <marcel.safont1983@gmail.com>
2026-03-02 07:58:36 +0100

    update scripts

---
+++ dist/server/ssr/__vite_rsc_assets_manifest.js
@@ -0,0 +1,29 @@
+export default {
+  "bootstrapScriptContent": "import(\"/assets/index-SUzedg-j.js\")",
+  "clientReferenceDeps": {
+    "f29e6e234fea": {
+      "js": [
+        "/assets/facade__virtual_vinext-rsc-entry-DCgoDtfO.js",
+        "/assets/framework-Ce_Irb5n.js",
+        "/assets/index-SUzedg-j.js"
+      ],
+      "css": []
+    },
+    "0deffcb8ffd7": {
+      "js": [
+        "/assets/facade__virtual_vinext-rsc-entry-DCgoDtfO.js",
+        "/assets/framework-Ce_Irb5n.js",
+        "/assets/index-SUzedg-j.js"
+      ],
+      "css": []
+    }
+  },
+  "serverResources": {
+    "app/layout.tsx": {
+      "js": [],
+      "css": [
+        "/assets/index-DcmnEmc1.css"
+      ]
+    }
+  }
+}
